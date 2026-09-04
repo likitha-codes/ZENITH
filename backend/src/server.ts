43a42d00f7b4a53e244ec996341f5e2a7be4790f
likitha <likithasrit@gmail.com>
2026-09-05 03:40:39 +0530

    Fix MongoDB DNS resolution

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,12 @@
+import dns from "dns";
 import dotenv from "dotenv";
+
 import app from "./app";
+
 import { connectDatabase } from "./config/database";
+
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(["8.8.8.8"]);
 
 dotenv.config();
 
@@ -9,7 +15,9 @@ const PORT = process.env.PORT || 5000;
 connectDatabase()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`ZENITH backend running on http://localhost:${PORT}`);
+      console.log(
+        `ZENITH backend running on http://localhost:${PORT}`
+      );
     });
   })
   .catch((error) => {
